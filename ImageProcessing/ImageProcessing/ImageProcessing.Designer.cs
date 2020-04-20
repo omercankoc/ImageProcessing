a89eadb31c8b3d4d96440da1dd683ab6c90de3e9
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2020-04-20 17:30:44 +0300

    Initial commit

---
+++ ImageProcessing/ImageProcessing/ImageProcessing.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace ImageProcessing
+{
+    partial class ImageProcessing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.In = new System.Windows.Forms.PictureBox();
+            this.Out = new System.Windows.Forms.PictureBox();
+            this.SelectImage = new System.Windows.Forms.Button();
+            this.GreyScaleFilter = new System.Windows.Forms.Button();
+            this.FilterRedComponent = new System.Windows.Forms.Button();
+            this.FilterGreenComponent = new System.Windows.Forms.Button();
+            this.FilterBlueComponent = new System.Windows.Forms.Button();
+            this.MovingAverageFilter = new System.Windows.Forms.Button();
+            this.MovingMedianFilter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.In)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Out)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // In
+            // 
+            this.In.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.In.Location = new System.Drawing.Point(12, 12);
+            this.In.Name = "In";
+            this.In.Size = new System.Drawing.Size(500, 500);
+            this.In.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.In.TabIndex = 0;
+            this.In.TabStop = false;
+            // 
+            // Out
+            // 
+            this.Out.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Out.Location = new System.Drawing.Point(730, 12);
+            this.Out.Name = "Out";
+            this.Out.Size = new System.Drawing.Size(500, 500);
+            this.Out.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Out.TabIndex = 1;
+            this.Out.TabStop = false;
+            // 
+            // SelectImage
+            // 
+            this.SelectImage.Location = new System.Drawing.Point(518, 11);
+            this.SelectImage.Name = "SelectImage";
+            this.SelectImage.Size = new System.Drawing.Size(206, 23);
+            this.SelectImage.TabIndex = 2;
+            this.SelectImage.Text = "Select Image";
+            this.SelectImage.UseVisualStyleBackColor = true;
+            this.SelectImage.Click += new System.EventHandler(this.SelectImage_Click);
+            // 
+            // GreyScaleFilter
+            // 
+            this.GreyScaleFilter.Location = new System.Drawing.Point(518, 40);
+            this.GreyScaleFilter.Name = "GreyScaleFilter";
+            this.GreyScaleFilter.Size = new System.Drawing.Size(206, 23);
+            this.GreyScaleFilter.TabIndex = 3;
+            this.GreyScaleFilter.Text = "Grey Scale Filter";
+            this.GreyScaleFilter.UseVisualStyleBackColor = true;
+            this.GreyScaleFilter.Click += new System.EventHandler(this.GreyScaleFilter_Click);
+            // 
+            // FilterRedComponent
+            // 
+            this.FilterRedComponent.Location = new System.Drawing.Point(518, 69);
+            this.FilterRedComponent.Name = "FilterRedComponent";
+            this.FilterRedComponent.Size = new System.Drawing.Size(206, 23);
+            this.FilterRedComponent.TabIndex = 4;
+            this.FilterRedComponent.Text = "Filter Red Component ";
+            this.FilterRedComponent.UseVisualStyleBackColor = true;
+            this.FilterRedComponent.Click += new System.EventHandler(this.FilterRedComponent_Click);
+            // 
+            // FilterGreenComponent
+            // 
+            this.FilterGreenComponent.Location = new System.Drawing.Point(518, 98);
+            this.FilterGreenComponent.Name = "FilterGreenComponent";
+            this.FilterGreenComponent.Size = new System.Drawing.Size(206, 23);
+            this.FilterGreenComponent.TabIndex = 5;
+            this.FilterGreenComponent.Text = "Filter Green Component ";
+            this.FilterGreenComponent.UseVisualStyleBackColor = true;
+            this.FilterGreenComponent.Click += new System.EventHandler(this.FilterGreenComponent_Click);
+            // 
+            // FilterBlueComponent
+            // 
+            this.FilterBlueComponent.Location = new System.Drawing.Point(518, 127);
+            this.FilterBlueComponent.Name = "FilterBlueComponent";
+            this.FilterBlueComponent.Size = new System.Drawing.Size(206, 23);
+            this.FilterBlueComponent.TabIndex = 6;
+            this.FilterBlueComponent.Text = "Filter Blue Component";
+            this.FilterBlueComponent.UseVisualStyleBackColor = true;
+            this.FilterBlueComponent.Click += new System.EventHandler(this.FilterBlueComponent_Click);
+            // 
+            // MovingAverageFilter
+            // 
+            this.MovingAverageFilter.Location = new System.Drawing.Point(518, 156);
+            this.MovingAverageFilter.Name = "MovingAverageFilter";
+            this.MovingAverageFilter.Size = new System.Drawing.Size(206, 23);
+            this.MovingAverageFilter.TabIndex = 7;
+            this.MovingAverageFilter.Text = "Moving Average Filter";
+            this.MovingAverageFilter.UseVisualStyleBackColor = true;
+            this.MovingAverageFilter.Click += new System.EventHandler(this.MovingAverageFilter_Click);
+            // 
+            // MovingMedianFilter
+            // 
+            this.MovingMedianFilter.Location = new System.Drawing.Point(518, 185);
+            this.MovingMedianFilter.Name = "MovingMedianFilter";
+            this.MovingMedianFilter.Size = new System.Drawing.Size(206, 23);
+            this.MovingMedianFilter.TabIndex = 8;
+            this.MovingMedianFilter.Text = "Moving Median Filter";
+            this.MovingMedianFilter.UseVisualStyleBackColor = true;
+            this.MovingMedianFilter.Click += new System.EventHandler(this.MovingMedianFilter_Click);
+            // 
+            // ImageProcessing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1242, 525);
+            this.Controls.Add(this.MovingMedianFilter);
+            this.Controls.Add(this.MovingAverageFilter);
+            this.Controls.Add(this.FilterBlueComponent);
+            this.Controls.Add(this.FilterGreenComponent);
+            this.Controls.Add(this.FilterRedComponent);
+            this.Controls.Add(this.GreyScaleFilter);
+            this.Controls.Add(this.SelectImage);
+            this.Controls.Add(this.Out);
+            this.Controls.Add(this.In);
+            this.Name = "ImageProcessing";
+            this.Text = "Image Processing";
+            ((System.ComponentModel.ISupportInitialize)(this.In)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Out)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox In;
+        private System.Windows.Forms.PictureBox Out;
+        private System.Windows.Forms.Button SelectImage;
+        private System.Windows.Forms.Button GreyScaleFilter;
+        private System.Windows.Forms.Button FilterRedComponent;
+        private System.Windows.Forms.Button FilterGreenComponent;
+        private System.Windows.Forms.Button FilterBlueComponent;
+        private System.Windows.Forms.Button MovingAverageFilter;
+        private System.Windows.Forms.Button MovingMedianFilter;
+    }
+}
+
---
+++ ImageProcessing/ImageProcessing/ImageProcessing.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageProcessing
+{
+    public partial class ImageProcessing : Form
+    {
+        public ImageProcessing()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog sfd = new OpenFileDialog();
+            sfd.Filter = "resimler|*.bmp|All Files|*.*";
+            if (sfd.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+            }
+            In.ImageLocation = sfd.FileName;
+        }
+
+        private void GreyScaleFilter_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(In.Image);
+            Bitmap grey = Grey(image);
+
+            Out.Image = grey;
+        }
+
+        private void FilterRedComponent_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(In.Image);
+            Bitmap red = Red(image);
+
+            Out.Image = red;
+        }
+
+        private void FilterGreenComponent_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(In.Image);
+            Bitmap green = Green(image);
+
+            Out.Image = green;
+        }
+
+        private void FilterBlueComponent_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(In.Image);
+            Bitmap blue = Blue(image);
+
+            Out.Image = blue;
+        }
+
+        private void MovingAverageFilter_Click(object sender, EventArgs e)
+        {
+            Color takeColor;
+            Bitmap InImage, OutImage;
+            InImage = new Bitmap(In.Image);
+
+            int width = InImage.Width;
+            int height = InImage.Height;
+
+            OutImage = new Bitmap(width, height);
+
+            int templateSize = 3;
+            int x, y, i, j, sumR, sumG, sumB, meanR, meanG, meanB;
+
+            for (x = (templateSize - 1) / 2; x < width - (templateSize - 1) / 2; x++)
+            {
+                for (y = (templateSize - 1) / 2; y < height - (templateSize - 1) / 2; y++)
+                {
+                    sumR = 0;
+                    sumG = 0;
+                    sumB = 0;
+                    for (i = -((templateSize - 1) / 2); i <= (templateSize - 1) / 2; i++)
+                    {
+                        for (j = -((templateSize - 1) / 2); j <= (templateSize - 1) / 2; j++)
+                        {
+                            takeColor = InImage.GetPixel(x + i, y + j);
+
+                            sumR = sumR + takeColor.R;
+                            sumG = sumG + takeColor.G;
+                            sumB = sumB + takeColor.B;
+                        }
+                    }
+
+                    meanR = sumR / (templateSize * templateSize);
+                    meanG = sumG / (templateSize * templateSize);
+                    meanB = sumB / (templateSize * templateSize);
+
+                    OutImage.SetPixel(x, y, Color.FromArgb(meanR, meanG, meanB));
+
+                }
+            }
+
+            Out.Image = OutImage;
+        }
+
+        private void MovingMedianFilter_Click(object sender, EventArgs e)
+        {
+            Color takeColor;
+            Bitmap inImage, OutImage;
+
+            inImage = new Bitmap(In.Image);
+
+            int width = inImage.Width;
+            int height = inImage.Height;
+
+            OutImage = new Bitmap(width, height);
+
+            int templateSize = 3;
+            int quantity = templateSize * templateSize;
+
+            int[] R = new int[quantity];
+            int[] G = new int[quantity];
+            int[] B = new int[quantity];
+            int[] Grey = new int[quantity];
+
+
+            int x, y, i, j;
+
+            for (x = (templateSize - 1) / 2; x < width - (templateSize - 1) / 2; x++)
+            {
+                for (y = (templateSize - 1) / 2; y < height - (templateSize - 1) / 2; y++)
+                {
+                    int k = 0;
+                    for (i = -((templateSize - 1) / 2); i <= (templateSize - 1) / 2; i++)
+                    {
+                        for (j = -((templateSize - 1) / 2); j <= (templateSize - 1) / 2; j++)
+                        {
+                            takeColor = inImage.GetPixel(x + i, y + j);
+                            R[k] = takeColor.R;
+                            G[k] = takeColor.G;
+                            B[k] = takeColor.B;
+                            Grey[k] = Convert.ToInt16(R[k] * 0.299 + G[k] * 0.587 + B[k] * 0.114);
+                            k++;
+                        }
+                    }
+
+                    int counter = 0;
+
+                    for (i = 0; i < quantity; i++)
+                    {
+                        for (j = i + 1; j < quantity; j++)
+                        {
+                            if (Grey[j] < Grey[i])
+                            {
+                                counter = Grey[i];
+                                Grey[i] = Grey[j];
+                                Grey[j] = counter;
+                                counter = R[i];
+                                R[i] = R[j];
+                                R[j] = counter;
+                                counter = G[i];
+                                G[i] = G[j];
+                                G[j] = counter;
+                                counter = B[i];
+                                B[i] = B[j];
+                                B[j] = counter;
+                            }
+                        }
+                    }
+
+                    OutImage.SetPixel(x, y, Color.FromArgb(R[(quantity - 1) / 2], G[(quantity - 1) / 2], B[(quantity - 1) / 2]));
+                }
+            }
+
+            Out.Image = OutImage;
+        }
+
+        private Bitmap Grey(Bitmap gbmp)
+        {
+            for (int i = 0; i < gbmp.Height - 1; i++)
+            {
+                for (int j = 0; j < gbmp.Width - 1; j++)
+                {
+                    int value = (gbmp.GetPixel(j, i).R + gbmp.GetPixel(j, i).G + gbmp.GetPixel(j, i).B) / 3;
+                    Color color;
+                    color = Color.FromArgb(value, value, value);
+                    gbmp.SetPixel(j, i, color);
+                }
+            }
+            return gbmp;
+        }
+
+        private Bitmap Red(Bitmap rbmp)
+        {
+            for (int i = 0; i < rbmp.Height; i++)
+            {
+                for (int j = 0; j < rbmp.Width; j++)
+                {
+                    int a = rbmp.GetPixel(j, i).A;
+                    int r = rbmp.GetPixel(j, i).R;
+                    Color color;
+                    color = Color.FromArgb(a, r, 0, 0);
+                    rbmp.SetPixel(j, i, color);
+                }
+
+            }
+            return rbmp;
+        }
+
+        private Bitmap Green(Bitmap gbmp)
+        {
+            for (int i = 0; i < gbmp.Height; i++)
+            {
+                for (int j = 0; j < gbmp.Width; j++)
+                {
+                    int a = gbmp.GetPixel(j, i).A;
+                    int g = gbmp.GetPixel(j, i).G;
+                    Color color;
+                    color = Color.FromArgb(a, 0, g, 0);
+                    gbmp.SetPixel(j, i, color);
+                }
+
+            }
+            return gbmp;
+        }
+
+        private Bitmap Blue(Bitmap bbmp)
+        {
+            for (int i = 0; i < bbmp.Height; i++)
+            {
+                for (int j = 0; j < bbmp.Width; j++)
+                {
+                    int a = bbmp.GetPixel(j, i).A;
+                    int b = bbmp.GetPixel(j, i).B;
+                    Color color;
+                    color = Color.FromArgb(a, 0, 0, b);
+                    bbmp.SetPixel(j, i, color);
+                }
+            }
+            return bbmp;
+        }
+    }
+}
